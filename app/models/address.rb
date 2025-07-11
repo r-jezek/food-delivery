bdbe61b6cdf897ee16dd49f5587f01f87e1a6601
Rostislav Ježek <214392935+r-jezek@users.noreply.github.com>
2025-07-12 00:22:10 +0200

    Add validations to Address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,5 @@
 class Address < ApplicationRecord
+  validates :city, :area_code, :street, :house_number, presence: true
+  validates :area_code, length: { maximum: 6 }
+  validates :house_number, numericality: { greater_than: 0 }
 end
