2163586d3f6afead682dd66f233b2eda745edda2
Rostislav Ježek <214392935+r-jezek@users.noreply.github.com>
2025-08-09 23:24:53 +0200

    Add validations to Menu Item

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,3 +1,6 @@
 class MenuItem < ApplicationRecord
   belongs_to :restaurant
+
+  validates :restaurant, :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
