05cdaf887d89a689f94f1beb9735ed88cb93ef28
Rostislav Ježek <214392935+r-jezek@users.noreply.github.com>
2025-08-09 21:45:32 +0200

    Add string representation to Address

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,8 @@ class Address < ApplicationRecord
   validates :city, :area_code, :street, :house_number, presence: true
   validates :area_code, length: { maximum: 6 }
   validates :house_number, numericality: { greater_than: 0 }
+
+  def to_s
+    "#{house_number} #{street}, #{city}, #{area_code}"
+  end
 end
