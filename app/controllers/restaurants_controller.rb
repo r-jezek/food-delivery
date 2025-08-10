148de435e3bee1ee1aeb1d9cf97793089de9cea0
Rostislav Ježek <214392935+r-jezek@users.noreply.github.com>
2025-08-10 23:19:31 +0200

    Create basic Restaurant page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,5 +4,8 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+    @is_open = @restaurant.opening_time.to_fs(:time) < Time.now.to_fs(:time) and @restaurant.closing_time.to_fs(:time) > Time.now.to_fs(:time)
+    @menu = @restaurant.menu_items
   end
 end
