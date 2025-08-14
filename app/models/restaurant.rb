23ac393d16f7f7c661982c8ae8e599fb5dc4e550
Rostislav Ježek <214392935+r-jezek@users.noreply.github.com>
2025-08-14 22:48:41 +0200

    Add cuisine enum and selector to Restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,6 @@ class Restaurant < ApplicationRecord
   belongs_to :address
   has_many :menu_items
 
+  enum :cuisine, { czech: "Czech", vietnamese: "Vietnamese" }
   validates :name, :address, :opening_time, :closing_time, :cuisine, presence: true
 end
