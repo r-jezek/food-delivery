f9044846ad1685c9fd67efe0de84f5de8a659173
Rostislav Ježek <214392935+r-jezek@users.noreply.github.com>
2025-08-16 00:32:04 +0200

    Add Cart page

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,11 +2,10 @@ class CartController < ApplicationController
   def add
     session[:cart] ||= []
     session[:cart] << params[:id]
-    puts(session[:cart])
-    puts("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa")
     redirect_back fallback_location: root_path
   end
 
   def index
+    @cart = MenuItem.where(id: session[:cart] || [])
   end
 end
