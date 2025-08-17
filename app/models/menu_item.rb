dd958573960203c9f831137714e898aa0a23838d
Rostislav Ježek <214392935+r-jezek@users.noreply.github.com>
2025-08-17 22:24:36 +0200

    Add relationships for Orders and Items

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,5 +1,7 @@
 class MenuItem < ApplicationRecord
   belongs_to :restaurant
+  has_many :order_items
+  has_many :orders, through: :order_items
 
   validates :restaurant, :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
