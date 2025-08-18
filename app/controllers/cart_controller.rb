b002df5ab5975981cf568ea4e22a851384a73b3b
Rostislav Ježek <214392935+r-jezek@users.noreply.github.com>
2025-08-18 23:58:19 +0200

    Save Cart total in session storage

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,18 +1,22 @@
 class CartController < ApplicationController
   def add
     session[:cart] ||= []
+    session[:cart_total] ||= 0
     session[:cart] << params[:id]
+    session[:cart_total] += MenuItem.find(params[:id]).price
     redirect_back fallback_location: root_path
   end
 
   def remove
     index = session[:cart].index(params[:id])
     session[:cart].delete_at(index) if index
+    session[:cart_total] -= MenuItem.find(params[:id]).price
     redirect_back fallback_location: root_path
   end
 
   def clear
     session[:cart] = []
+    session[:cart_total] = 0
     redirect_back fallback_location: root_path
   end
 
