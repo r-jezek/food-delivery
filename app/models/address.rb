6796fb018f6633c3eff2a019250f9358b0823193
Rostislav Ježek <214392935+r-jezek@users.noreply.github.com>
2025-08-21 23:51:51 +0200

    Fix Order and Address cardinality

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,6 @@
 class Address < ApplicationRecord
+  has_many :orders
+
   validates :city, :area_code, :street, :house_number, presence: true
   validates :area_code, length: { maximum: 6 }
   validates :house_number, numericality: { greater_than: 0 }
