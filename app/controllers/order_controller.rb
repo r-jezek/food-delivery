fa9936aca415f7340401ddc03ac4458d44f991ea
Rostislav Ježek <214392935+r-jezek@users.noreply.github.com>
2025-08-21 23:57:02 +0200

    Update Order creation

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -4,5 +4,21 @@ class OrderController < ApplicationController
   end
 
   def create
+    @address = Address.new(city: order_params[:city], area_code: order_params[:area_code], street: order_params[:street], house_number: order_params[:house_number])
+    if not @address.save
+      render :index, status: :unprocessable_entity
+    end
+
+    @order = Order.new(address_id: @address.id, total_price: session[:cart_total], date_ordered: Time.now)
+    if not @order.save
+      render :index, status: :unprocessable_entity
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  def order_params
+    params.expect(order: [ :city, :street, :area_code, :house_number, :total ])
   end
 end
